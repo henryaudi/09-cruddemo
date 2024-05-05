7e6aee2f3e17ed0d7611f6f3c0fca56b5b2a3a1f
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-05-05 15:39:10 -0500

    Updated CruddemoApplication

--- src/main/java/com/springproject/cruddemo/CruddemoApplication.java
+++ src/main/java/com/springproject/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.springproject.cruddemo;
 
+import com.springproject.cruddemo.dao.AppDAO;
+import com.springproject.cruddemo.entity.Instructor;
+import com.springproject.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +16,32 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Chad", "Darby", "darby@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"https://www.google.com",
+						"GOOOOOGLE!"
+				);
+
+		// associate one-to-one relationship.
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		System.out.println("Saving the instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
 	}
 
 }
