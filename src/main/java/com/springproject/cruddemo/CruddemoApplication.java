17c9997500dded12f7fbb7bbde6ae158e53b120e
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-05-05 15:54:44 -0500

    Updated findInstructorById feature

--- src/main/java/com/springproject/cruddemo/CruddemoApplication.java
+++ src/main/java/com/springproject/cruddemo/CruddemoApplication.java
@@ -19,21 +19,45 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			createInstructor(appDAO);
+			findInstructor(appDAO);
+
+//			createInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
 	}
 
 	private void createInstructor(AppDAO appDAO) {
 
+//		// create the instructor
+//		Instructor tempInstructor =
+//				new Instructor("Chad", "Darby", "darby@gmail.com");
+//
+//		// create the instructor detail
+//		InstructorDetail tempInstructorDetail =
+//				new InstructorDetail(
+//						"https://www.google.com",
+//						"GOOOOOGLE!"
+//				);
+
 		// create the instructor
 		Instructor tempInstructor =
-				new Instructor("Chad", "Darby", "darby@gmail.com");
+				new Instructor("Ronald", "Dickson", "dicksonr@gmail.com");
 
 		// create the instructor detail
 		InstructorDetail tempInstructorDetail =
 				new InstructorDetail(
-						"https://www.google.com",
-						"GOOOOOGLE!"
+						"https://www.yahoo.com",
+						"Guitar"
 				);
 
 		// associate one-to-one relationship.
